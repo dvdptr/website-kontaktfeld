0e1058d40c92aca8ffd2d8bb7e90889f4f3ac982
dvdptr <info@davidebner.de>
2020-06-10 11:23:48 +0200

    appFooter: add content; TODO: replace email and phone, add impressum

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,11 +1,33 @@
 <template>
-  <div class="border-b border-gray-300 pb-12">
-    <p>Footer Section</p>
+  <div class="border-b border-gray-300 py-12 text-sm text-gray-700">
+    <div class="flex justify-evenly">
+      <div>
+        <Logo class="h-8" />
+      </div>
+      <div class>
+        <p class="underline">Anschrift</p>
+        <p>Paulinstr. 75</p>
+        <p>54292 Trier</p>
+      </div>
+      <div class>
+        <p class="underline">Kontakt</p>
+        <p>Telefon: 9876</p>
+        <p>e-Mail: se@gestaltberatung-trier.de</p>
+      </div>
+    </div>
+    <div class="text-center pt-12 text-sm text-gray-600">
+      <p>c Trier, 2020 | Impressum</p>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import Logo from "@/components/Logo";
+export default {
+  components: {
+    Logo
+  }
+};
 </script>
 
 <style>
