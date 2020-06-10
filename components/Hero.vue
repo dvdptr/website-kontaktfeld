4693fc2e024fdbabfe0465b96c6c2becf695b395
dvdptr <info@davidebner.de>
2020-06-10 06:14:08 +0200

    hero: add content and adjust headline and text size

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,6 +1,18 @@
 <template>
-  <div class="border-b border-gray-300 pb-12">
-    <p>Hero Section</p>
+  <div class="border-b border-gray-300 pb-12 pt-16 text-center">
+    <div class="w-2/3 mx-auto">
+      <div>
+        <p class="font-bold text-5xl leading-tight pb-8">
+          Entdecken Sie sich neu.
+          <br />Begegnen Sie Ihrem Selbst.
+        </p>
+      </div>
+      <div class>
+        <p
+          class="text-gray-800"
+        >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic optio officia at, velit ea vitae delectus magni reiciendis distinctio. Voluptatibus?</p>
+      </div>
+    </div>
   </div>
 </template>
 
