d26be114e5619ea97e0a1d1cb66f443e7f825c14
dvdptr <info@davidebner.de>
2020-06-10 05:48:28 +0200

    appHeader: add nav bar

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="border-b border-gray-300 pb-12">
-    <Logo class="h-10" />
+  <div class="border-b border-gray-300 pb-12 pt-10 flex justify-between items-center">
+    <div>
+      <Logo class="h-10" />
+    </div>
+    <div>
+      <nav>Menu</nav>
+    </div>
   </div>
 </template>
 
