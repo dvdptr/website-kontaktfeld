7f1b56452dba29257dcb3e7078f5373521906959
dvdptr <info@davidebner.de>
2020-06-10 05:38:33 +0200

    clean project and replace logo

---
+++ components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Logo class="h-10" />
+  </div>
+</template>
+
+<script>
+import Logo from "@/components/Logo";
+export default {
+  components: {
+    Logo
+  }
+};
+</script>
+
+<style>
+</style>
